2179b4d949da9f8bcf50058a8aa8fa95d09dee8a
JoaoEler <jgelermendes@gmail.com>
2020-04-27 15:44:01 -0300

    like/dislike check

--- src/services/user/indexCons.js
+++ src/services/user/indexCons.js
@@ -4,6 +4,10 @@ module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query;
 
+        const { id } = request.headers;
+
+        const loggedUser = await connection('investidores').where('id', id);
+
         const [ count ] = await connection('consultores').count();
 
         const consultores = await connection('consultores')
@@ -11,6 +15,14 @@ module.exports = {
             .offset(page - 1 )
             .select('consultores.pic', 'consultores.name', 'consultores.bio', 'consultores.interesses');
         response.header('X-Total-Count', count['count(*)']);
+
+        if (loggedUser.includes('likes', consultores[0].id)){
+            delete consultores[0];
+        }
+
+        if (loggedUser.includes('dislikes', consultores[0].id)){
+            delete consultores[0];
+        }
 
         return response.json(consultores);
     }
