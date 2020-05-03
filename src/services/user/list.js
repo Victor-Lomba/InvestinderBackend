8b525f0e62957890b0898177c9d2e83ec136437d
victor <victot21@hotmail.com>
2020-05-02 22:30:47 -0300

    list possible matches

--- src/services/user/list.js
+++ src/services/user/list.js
@@ -2,17 +2,42 @@ const connection = require('../../database/connection');
 
 module.exports = {
     async index(request, response) {
+    
+        if(!request.headers.authorization)return response.status(401).send();
+        const uid = request.headers.authorization;
 
-        const uid = request.headers;
+        const usuario = await connection('investidores').where('id', uid).select('*');
 
-        const usuario = await connection('investidores').where('id', uid).select('*'); 
-
-        const likes = usuario.likes;
+        const likes = usuario.likes.split(' ');
         const dislikes = usuario.dislikes;
+
+        
+        
 
         const consultor = await connection('consultores').select('*');
 
 
-        return response.json(consultor);
+        var resposta = [];
+        if(!likes == null){
+            for(let y of consultor){
+            let liked = false;
+                for(let i of likes){
+                    if(i == y.id)
+                    {
+                        liked = true;
+                    }
+                }
+                if(!liked){resposta += y}
+            }
+        }
+        if(!dislikes == null){}
+        resposta = consultor[0];
+       
+
+            
+        
+
+
+        return response.json(resposta);
     }
 }
